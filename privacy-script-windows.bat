a16c1271c958b5ab43f7ee0b8e8b33301b76fa88
NodeMixaholic <sam@sllord.info>
2023-04-24 14:49:35 -0500

    Update privacy-script-windows.bat

--- privacy-script-windows.bat
+++ privacy-script-windows.bat
@@ -1627,19 +1627,6 @@ echo Changing TTL for limitless tethering on an unlimited plan...
 netsh int ipv4 set glob defaultcurhoplimit=65
 netsh int ipv6 set glob defaultcurhoplimit=65
 
-@echo off
-
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-echo Requesting administrative privileges...
-goto UACPrompt
-) else ( goto gotAdmin )
-:UACPrompt
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs"
-exit /B
 :gotAdmin
 if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
 pushd "%CD%"
